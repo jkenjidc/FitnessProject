58e11339de328a77eef46413c5353c755f8d9c10
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-09-06 13:22:11 -0400

    Initial Commit

---
+++ FitnessProject/FitnessProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessProjectApp.swift
+//  FitnessProject
+//
+//  Created by Kenji Dela Cruz on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
