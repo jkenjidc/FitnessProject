a0f1b781ffd90a45dc50df9ea3537bdffe68309d
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-10-02 17:21:34 -0400

    initial testing for routine creation

---
+++ FitnessProjectTests/RoutineCreationTests.swift
@@ -0,0 +1,37 @@
+//
+//  RoutineCreationTests.swift
+//  FitnessProjectTests
+//
+//  Created by Kenji Dela Cruz on 10/2/24.
+//
+
+import XCTest
+@testable import FitnessProject
+
+final class RoutineCreationTests: XCTestCase {
+    let testEmail =  "testingEmail@test.com"
+    let password =  "testPassword123"
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        try await AuthManager.shared.signInUser(email: testEmail, password: password)
+        try await DataManager.shared.loadUser()
+    }
+    
+    
+    func testRoutineCreation() async throws {
+        let set = ExerciseSet(weight: 50, reps: 8)
+        let exercise = Exercise(name: "Sample Exercise", sets: [set])
+        let routine = Routine(name: "Test Routine", daysToDo: [], datesDone: [], exercises: [exercise])
+        try await DataManager.shared.addRoutine(routine: routine)
+        let user = await DataManager.shared.user
+        XCTAssertFalse(user.routines.isEmpty)
+    }
+//    
+//    override func tearDown() async throws{
+//        
+//    }
+    
+    
+
+}
