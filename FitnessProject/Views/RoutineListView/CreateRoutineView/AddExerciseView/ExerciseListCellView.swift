bd333c21fa161a3f1f7db40c79be5707430677ba
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-10-14 17:54:48 -0400

    added completed button and pause button for timer

--- FitnessProject/Views/RoutineListView/CreateRoutineView/AddExerciseView/ExerciseListCellView.swift
+++ FitnessProject/Views/RoutineListView/CreateRoutineView/AddExerciseView/ExerciseListCellView.swift
@@ -10,36 +10,51 @@ import SwiftUI
 struct exerciseSetListRowView: View {
     @Binding var exercise: Exercise
     @Binding var exerciseSet: ExerciseSet
+    @State private var completed = false
+    var timerMode: Bool
     var body: some View {
         Group{
-            Text(exercise.getSetIndex(exerciseSet: exerciseSet))
-            TextField("\(exerciseSet.weight)", value: $exerciseSet.weight, format: .number)
-                .multilineTextAlignment(.center)
-            TextField("\(exerciseSet.reps)", value: $exerciseSet.reps, format: .number)
-                .multilineTextAlignment(.center)
             Button {
                 exercise.deleteSet(exerciseSet: exerciseSet)
-                
             } label: {
                 Image(systemName: "minus.square.fill")
                     .foregroundStyle(Color.red)
             }
             .buttonStyle(.plain)
+            Text(exercise.getSetIndex(exerciseSet: exerciseSet))
+            TextField("\(exerciseSet.weight)", value: $exerciseSet.weight, format: .number)
+                .multilineTextAlignment(.center)
+            TextField("\(exerciseSet.reps)", value: $exerciseSet.reps, format: .number)
+                .multilineTextAlignment(.center)
+            if timerMode {
+                Image(systemName: ( completed ? "checkmark.square.fill" : "square"))
+                    .foregroundStyle(.green)
+                    .onTapGesture {
+                        completed.toggle()
+                    }
+            } else {
+                Text("")
+                    .frame(width: 25)
+            }
         }
     }
 }
 
 struct ExerciseListCellView: View {
     @Binding var exercise: Exercise
+    var screenMode: ScreenMode
+    var timerMode: Bool {
+        return screenMode == .timer
+    }
     var deleteExercise: (Exercise) -> Void
-    
     let columns = [
         GridItem(.fixed(80)),
         GridItem(.fixed(80)),
         GridItem(.fixed(80)),
+        GridItem(.fixed(80)),
         GridItem(.fixed(80))
+        
     ]
-    
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0){
@@ -52,22 +67,24 @@ struct ExerciseListCellView: View {
                         }
                     }
                     .padding(.leading, 30)
-
+                
                 Text(exercise.name)
                     .frame(maxWidth: .infinity)
                     .padding()
-                
             }
+            
             LazyVGrid(columns: columns){
+                Text("")
                 Text("Sets")
                 Text("lbs")
                 Text("Reps")
                 Text("")
                 ForEach($exercise.sets){ exerciseSet in
-                    exerciseSetListRowView(exercise: $exercise, exerciseSet: exerciseSet)
+                    exerciseSetListRowView(exercise: $exercise, exerciseSet: exerciseSet, timerMode: timerMode)
                 }
             }
             .padding(.bottom, 15)
+            
             Button{
                 addSet()
             } label: {
@@ -79,7 +96,6 @@ struct ExerciseListCellView: View {
                     .padding(.horizontal, 30)
             }
             .buttonStyle(.plain)
-
         }
         .padding(.bottom, 5)
     }
@@ -88,14 +104,13 @@ struct ExerciseListCellView: View {
         let lastWeight = exercise.sets.last?.weight ?? 0
         let lastRep = exercise.sets.last?.reps ?? 0
         exercise.sets.append(ExerciseSet(weight: lastWeight, reps: lastRep))
-
     }
 }
 
 #Preview {
     @State var exercise = Exercise(name: "Exercise 1", sets: [ExerciseSet(weight: 0, reps: 0)])
     func dummyfunc(exerise: Exercise){}
-    return ExerciseListCellView(exercise: $exercise, deleteExercise: dummyfunc.self )
+    return ExerciseListCellView(exercise: $exercise, screenMode: .creation, deleteExercise: dummyfunc.self)
         .preferredColorScheme(.dark)
 }
 
