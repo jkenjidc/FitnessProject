5f8cf82b9ac43c18f410dfbe5479c6915e82344d
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-10-31 16:53:11 -0400

    code cleanup

--- FitnessProject/Views/ProfileView/ProfileViewModel.swift
+++ FitnessProject/Views/ProfileView/ProfileViewModel.swift
@@ -17,23 +17,6 @@ extension ProfileView {
         var selectedItem: PhotosPickerItem?
         var profileImage: Image?
         
-        func loadImageFromDirectory() {
-            //loads profile picture from local cache directory
-            if profileImage == nil {
-                let path = getDocumentsDirectory().appending(path: "profileImage.jpeg")
-                if FileManager.default.fileExists(atPath: path.path()){
-                    print("file exists")
-                } else {
-                    print("file doesnt exist")
-                }
-                if let UIImage = UIImage(contentsOfFile: path.path()){
-                    profileImage = Image(uiImage: UIImage)
-                } else {
-                    print("error loading image")
-                }
-            }
-        }
-        
         func signOut(pop: () -> Void) {
             do {
                 try AuthManager.shared.signOut()
@@ -53,44 +36,33 @@ extension ProfileView {
             }
         }
         
-        func getDocumentsDirectory() -> URL {
-            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            let documentsDirectory = paths[0]
-            return documentsDirectory
-        }
-        
-        func loadImage() {
+        func loadImageFromPicker() {
             Task {
-                //loads selected image into the profileImage view
-                if let loaded = try? await selectedItem?.loadTransferable(type: Image.self){
-                    profileImage = loaded
-                } else {
-                    print("Error")
-                }
-                
                 if let loadedData = try? await selectedItem?.loadTransferable(type: Data.self){
-                    //uploads profile image to firebase storage
                     guard let inputImage = UIImage(data: loadedData) else { return }
-                    try await DataManager.shared.uploadImage(image: inputImage)
-                    
-                    //saves UIImage jpeg data for local caching
-                    if let data = inputImage.jpegData(compressionQuality: 0.8) {
-                        let filename = getDocumentsDirectory().appendingPathComponent("profileImage.jpeg")
-                        do{
-                            try data.write(to: filename)
-                            print("successfully saved at \(filename)")
-                        } catch {
-                            print("error saving image")
-                        }
-                    }
-                }else {
-                    print("error")
+                    profileImage = Image(uiImage: inputImage)
+                    await uploadAndCacheProfileImage(image: inputImage)
                 }
             }
         }
         
-        func uploadPhoto() {
-            
+        func loadImageFromDirectory() {
+            //loads profile picture from local cache directory
+            if profileImage == nil {
+                let fileName = DataManager.shared.getDocumentsDirectory().appending(path: FileNames.profileImage.rawValue)
+                if let UIImage = UIImage(contentsOfFile: fileName.path()){
+                    profileImage = Image(uiImage: UIImage)
+                }
+            }
+        }
+        
+        func uploadAndCacheProfileImage(image: UIImage) async {
+            do {
+                try await DataManager.shared.uploadImage(image: image)
+                try DataManager.shared.cacheImage(from: image)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
