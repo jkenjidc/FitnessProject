18aa4dea39a0ba35d3c0afce6941fe50372188df
Kenji Dela Cruz <kenji.delacruz@willowtreeapps.com>
2025-04-09 22:08:35 -0400

    added DTO and Model

---
+++ FitnessProject/DTOs/ExerciseDTO.swift
@@ -0,0 +1,19 @@
+//
+//  ExerciseDTO.swift
+//  FitnessProject
+//
+//  Created by Kenji Dela Cruz on 4/9/25.
+//
+
+import Foundation
+
+struct ExerciseDTO: Decodable {
+    let bodyPart: String
+    let equipment: String
+    let gifUrl: URL
+    let id: String
+    let name: String
+    let target: String
+    let secondaryMuscles: [String]
+    let instructions: [String]
+}
