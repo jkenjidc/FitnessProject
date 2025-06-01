5fd4a9f29cf0454b339c34470d1bfeb0d1677390
Kenji Dela Cruz <kenjidckenji@gmail.com>
2025-06-01 15:57:25 -0400

    Added theming to screen and timer functionality

--- FitnessProject/Views/ComponentViews/CurvedHeaderView.swift
+++ FitnessProject/Views/ComponentViews/CurvedHeaderView.swift
@@ -10,21 +10,18 @@ import SwiftUI
 struct CurvedHeaderView: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-
-        // Start from top-left
+        let curveDepth: CGFloat = 28
         path.move(to: CGPoint(x: 0, y: 0))
-
-        // Draw lines to form the rectangle
         path.addLine(to: CGPoint(x: rect.maxX, y: 0))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - 50))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - curveDepth))
 
-        // Add the curved bottom
+        // Adjust the curve
+        let controlPoint = CGPoint(x: rect.midX , y: rect.maxY + curveDepth)
         path.addQuadCurve(
-            to: CGPoint(x: 0, y: rect.maxY - 50),
-            control: CGPoint(x: rect.width / 2, y: rect.maxY)
+            to: CGPoint(x: 0, y: rect.maxY - curveDepth),
+            control: controlPoint
         )
 
-        // Close the path
         path.closeSubpath()
 
         return path
--- FitnessProject/Views/TimerScreen/TimerScreen.swift
+++ FitnessProject/Views/TimerScreen/TimerScreen.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct TimerScreen: View {
+    @Environment(Router.self) var router
     let routine: Routine
     var body: some View {
         VStack {
             TimerHeader(routineName: routine.name)
             TimerExerciseList(exercises: routine.exercises)
         }
+        .navigationBarBackButtonHidden(true)
+        .toolbar{
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    router.pop()
+                } label: {
+                    Text("Cancel")
+                        .foregroundStyle(.red)
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    router.pop()
+                } label: {
+                    Text("finish")
+                        .tint(.green)
+                }
+            }
+        }
+
     }
 }
 
 #Preview {
     TimerScreen(routine: .example[0])
         .preferredColorScheme(.dark)
+        .environment(Router())
 }
